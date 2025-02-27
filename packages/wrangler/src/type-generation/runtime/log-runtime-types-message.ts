7e0c6a19e3443b3903da579d58746214b4f1642d
emily-shen <69125074+emily-shen@users.noreply.github.com>
2025-02-27 20:08:47 +0000

    pr feedback

--- packages/wrangler/src/type-generation/runtime/log-runtime-types-message.ts
+++ packages/wrangler/src/type-generation/runtime/log-runtime-types-message.ts
@@ -1,6 +1,5 @@
 import { existsSync } from "node:fs";
 import chalk from "chalk";
-import { dedent } from "ts-dedent";
 import { logger } from "../../logger";
 
 /**
@@ -15,7 +14,6 @@ export function logRuntimeTypesMessage(
 		type.startsWith("@cloudflare/workers-types")
 	);
 	const isNodeTypesInstalled = tsconfigTypes.find((type) => type === "node");
-	const updatedTypesString = buildUpdatedTypesString(tsconfigTypes, outFile);
 	const maybeHasOldRuntimeFile = existsSync("./.wrangler/types/runtime.d.ts");
 	if (maybeHasOldRuntimeFile) {
 		logAction("Remove the old runtime.d.ts file");
@@ -37,24 +35,12 @@ export function logRuntimeTypesMessage(
 		);
 		logger.log("");
 	}
-	if (updatedTypesString) {
-		logAction("Update your tsconfig.json to include the generated types");
-		logger.log(
-			chalk.dim(dedent`
-				{
-					"compilerOptions": {
-						"types": ${updatedTypesString}
-					}
-				}
-		`)
-		);
-		logger.log("");
-	}
+
 	if (isNodeCompat && !isNodeTypesInstalled) {
-		logAction("Install types@node");
+		logAction("Install @types/node");
 		logger.log(
 			chalk.dim(
-				`Since you have the \`nodejs_compat\` flag, you should install Node.js types by running "npm i --save-dev ${isWorkersTypesInstalled ? '@types/node@20.8.3".\nFor more details: https://developers.cloudflare.com/workers/languages/typescript/#known-issues' : '@types/node".'}`
+				`Since you have the \`nodejs_compat\` flag, you should install Node.js types by running "npm i --save-dev @types/node".`
 			)
 		);
 		logger.log("");
@@ -64,29 +50,6 @@ export function logRuntimeTypesMessage(
 		`📖 Read about runtime types\n` +
 			`${chalk.dim("https://developers.cloudflare.com/workers/languages/typescript/#generate-types")}`
 	);
-}
-
-/**
- * Constructs a string representation of the existing types array with the new types path appended to.
- * It removes any existing types that are no longer relevant.
- */
-function buildUpdatedTypesString(
-	types: string[],
-	newTypesPath: string
-): string | null {
-	if (types.some((type) => type.includes(newTypesPath))) {
-		return null;
-	}
-
-	const updatedTypesArray = types
-		.filter(
-			(type) =>
-				!type.startsWith("@cloudflare/workers-types") &&
-				!type.includes("./wrangler/types/runtime.d.ts")
-		)
-		.concat([newTypesPath]);
-
-	return JSON.stringify(updatedTypesArray);
 }
 
 const logAction = (msg: string) => {
